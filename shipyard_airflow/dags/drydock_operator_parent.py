4635bb232efb4af6d7614f220f152dab13bb1517
Anthony Lin <anthony.jclin@gmail.com>
2017-08-12 04:46:00 +0000

    Shipyard DryDock Operator
    - Uses drydock_client
    - Added workflow to get keystone token
    - Added error handling for workflow
    - Covers the following flow
      1. create_drydock_client
      2. get_design_id
      3. load_parts (drydock)
      4. load_parts (promenade)
      5. verify_site
      6. prepare_site
      7. prepare_node
      8. deploy_node
    
    Current testing is done against a drydock container
    on local host machine using hard-coded token as the
    DryDock Helm Chart with Keystone integration is being
    reviewed at the moment.  Integration testing with keystone
    token will be done when the DryDock Chart is merged.
    
    Change-Id: Ic5eda099717de6f948afacf9afa07ea5dad3ba7f

---
+++ shipyard_airflow/dags/drydock_operator_parent.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+### DryDock Operator Parent Dag
+"""
+import airflow
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.subdag_operator import SubDagOperator
+from drydock_operator_child import sub_dag
+
+parent_dag_name = 'drydock_operator_parent'
+child_dag_name = 'drydock_operator_child'
+
+args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(1),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=1),
+    'provide_context': True
+}
+
+main_dag = DAG(
+    dag_id=parent_dag_name,
+    default_args=args,
+    schedule_interval=None,
+    start_date=airflow.utils.dates.days_ago(1),
+    max_active_runs=1
+)
+
+subdag = SubDagOperator(
+    subdag=sub_dag(parent_dag_name, child_dag_name, args, main_dag.schedule_interval),
+    task_id=child_dag_name,
+    default_args=args,
+    dag=main_dag)
