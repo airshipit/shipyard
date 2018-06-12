bf22828f5283233314240e604c22381efb224102
Scott Hussey <sh8121@att.com>
2018-06-12 16:06:02 -0500

    Updates to shipyard cli wrapper
    - Don't change directory so that the volume mounted in the
      container at /home/shipayard/host is predictable
    - Update image default to point at airshipit repo
    
    Change-Id: If8b0988925921d7b54ec74ac73e12289d89d392e

--- tools/shipyard_docker_base_command.sh
+++ tools/shipyard_docker_base_command.sh
@@ -19,7 +19,7 @@
 # before executing this script if they differ from the default values.
 #
 NAMESPACE="${NAMESPACE:-ucp}"
-SHIPYARD_IMAGE="${SHIPYARD_IMAGE:-quay.io/attcomdev/shipyard:latest}"
+SHIPYARD_IMAGE="${SHIPYARD_IMAGE:-quay.io/airshipit/shipyard:master}"
 
 # Define Base Docker Command
 base_docker_command=$(cat << EndOfCommand
