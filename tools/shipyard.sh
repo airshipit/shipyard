75c1ddba28b3bb168b9ba5d52598ae408461e837
Drew Walters <drewwalters96@gmail.com>
2018-08-27 15:11:17 +0000

    Add no_proxy environment variable to CLI script
    Currently, the shipyard.sh script does not set the no_proxy
    environment variable for the Shipyard container. Invoking the CLI
    to perform actions results in a 404 error unless the variable is set.
    This change adds localhost and .svc.cluster.local to the no_proxy
    environment variable.
    
    Change-Id: Ie0ae52d4ecb51eafe983a7a452b61a01c2c9426e

--- tools/shipyard.sh
+++ tools/shipyard.sh
@@ -45,6 +45,7 @@ base_docker_command=$(cat << EndOfCommand
 sudo docker run -t --rm --net=host
 -e http_proxy=${HTTP_PROXY}
 -e https_proxy=${HTTPS_PROXY}
+-e no_proxy=${NO_PROXY:-127.0.0.1,localhost,.svc.cluster.local}
 -e OS_AUTH_URL=${OS_AUTH_URL:-http://keystone.${NAMESPACE}.svc.cluster.local:80/v3}
 -e OS_USERNAME=${OS_USERNAME:-shipyard}
 -e OS_USER_DOMAIN_NAME=${OS_USER_DOMAIN_NAME:-default}
