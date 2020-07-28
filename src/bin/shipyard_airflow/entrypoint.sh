a5e57879ab318884493f9b0580c410adafc2e11a
Mahmoudi, Ahmad (am495p) <ahmad.mahmoudi@att.com>
2020-07-28 19:38:59 +0000

    Override uwsgi default config
    - Overrode uwsgi default configs to improve stability and performance.
    - Increased mas number of worker processes to increase capacity and
      performance.
    - Enabled uwsgi cheaper subsystem to scale worker processes dynamically.
    - Uplifted uwsgi to the latest release to bring bug fixes and
      improvements since 2018.
    
    Upgraded uwsgi to bring in bug fixes since 2018.
    
    For background information for this change please see:
    https://uwsgi-docs.readthedocs.io/en/latest/ThingsToKnow.html
    
    Change-Id: If067e9786e9dbbd39ef832dea6f51aa5523af4d7

--- src/bin/shipyard_airflow/entrypoint.sh
+++ src/bin/shipyard_airflow/entrypoint.sh
@@ -20,19 +20,57 @@ if [ "$1" = 'server' ]; then
     PORT=${PORT:-9000}
     HTTP_TIMEOUT=${HTTP_TIMEOUT:-600}
     # Number of uWSGI workers to handle API request
-    SHIPYARD_API_WORKERS=${SHIPYARD_API_WORKERS:-"4"}
+    SHIPYARD_API_WORKERS=${SHIPYARD_API_WORKERS:-"16"}
     #Threads per worker
     SHIPYARD_API_THREADS=${SHIPYARD_API_THREADS:-"1"}
+    # use uwsgi cheaper-busyness plugin for dynamic worker scaling
+    SHIPYARD_API_CHEAPER_ALGO=${SHIPYARD_API_CHEAPER_ALGO:-"busyness"}
+    # Minimum number of workers allowed
+    SHIPYARD_API_CHEAPER=${SHIPYARD_API_CHEAPER:-"4"}
+    # Number of workers created at startup
+    SHIPYARD_API_CHEAPER_INITIAL=${SHIPYARD_API_CHEAPER_INITIAL:-"8"}
+    # How many workers to spawn each time
+    SHIPYARD_API_CHEAPER_STEP=${SHIPYARD_API_CHEAPER_STEP:-"4"}
+    # Length of a busyness cycle in seconds
+    SHIPYARD_API_CHEAPER_OVERLOAD=${SHIPYARD_API_CHEAPER_OVERLOAD:-"1"}
+    # How many cycles to wait before killing workers due to low load
+    SHIPYARD_API_CHEAPER_BUSYNESS_MULTIPLIER=${SHIPYARD_API_CHEAPER_BUSYNESS_MULTIPLIER:-"60"}
+    # Below this threshold, kill workers (if stable for multiplier cycles)
+    SHIPYARD_API_CHEAPER_BUSYNESS_MIN=${SHIPYARD_API_CHEAPER_BUSYNESS_MIN:-"20"}
+    # Above this threshold, spawn new workers
+    SHIPYARD_API_CHEAPER_BUSYNESS_MAX=${SHIPYARD_API_CHEAPER_BUSYNESS_MAX:-"75"}
+    # Spawn emergency workers if more than this many requests are waiting in the queue
+    SHIPYARD_API_CHEAPER_BUSYNESS_BACKLOG_ALERT=${SHIPYARD_API_CHEAPER_BUSYNESS_BACKLOG_ALERT:-"40"}
+    # How many emergegency workers to create if there are too many requests in the queue
+    SHIPYARD_API_CHEAPER_BUSYNESS_BACKLOG_STEP=${SHIPYARD_API_CHEAPER_BUSYNESS_BACKLOG_STEP:-"1"}
     # Start shipyard application
+    # for more info about these default overrides please read
+    # https://uwsgi-docs.readthedocs.io/en/latest/ThingsToKnow.html
     exec uwsgi \
         --http :${PORT} \
         --paste config:/etc/shipyard/api-paste.ini \
         --enable-threads \
         -L \
         --pyargv "--config-file /etc/shipyard/shipyard.conf" \
-        --threads $SHIPYARD_API_THREADS \
-        --workers $SHIPYARD_API_WORKERS \
-        --http-timeout ${HTTP_TIMEOUT}
+        --threads ${SHIPYARD_API_THREADS} \
+        --workers ${SHIPYARD_API_WORKERS} \
+        --http-timeout ${HTTP_TIMEOUT} \
+        --strict \
+        --master \
+        --vacuum \
+        --single-interpreter \
+        --die-on-term \
+        --need-app \
+        --cheaper-algo ${SHIPYARD_API_CHEAPER_ALGO} \
+        --cheaper ${SHIPYARD_API_CHEAPER} \
+        --cheaper-initial ${SHIPYARD_API_CHEAPER_INITIAL} \
+        --cheaper-step ${SHIPYARD_API_CHEAPER_STEP} \
+        --cheaper-overload ${SHIPYARD_API_CHEAPER_OVERLOAD} \
+        --cheaper-busyness-multiplier ${SHIPYARD_API_CHEAPER_BUSYNESS_MULTIPLIER} \
+        --cheaper-busyness-min ${SHIPYARD_API_CHEAPER_BUSYNESS_MIN} \
+        --cheaper-busyness-max ${SHIPYARD_API_CHEAPER_BUSYNESS_MAX} \
+        --cheaper-busyness-backlog-alert ${SHIPYARD_API_CHEAPER_BUSYNESS_BACKLOG_ALERT} \
+        --cheaper-busyness-backlog-step ${SHIPYARD_API_CHEAPER_BUSYNESS_BACKLOG_STEP}
 else
     CMD="shipyard"
     # Execute shipyard command
