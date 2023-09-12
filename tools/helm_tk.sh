f3ea82daecfb16b0b11d4b2ec8aa9607aa612d2c
Anselme, Schubert (sa246v) <sa246v@att.com>
2023-09-12 09:48:02 -0400

    Deprecating the Ingress Class Annotation
    Upgrading htk to version 0.2.55, which deprecates the ingress class
    annotation (kubernetes.io/ingress.class) with .spec.ingressClassName
    
    https://review.opendev.org/c/openstack/openstack-helm-infra/+/891720
    
    Change-Id: I392b9ebaebc696ae07a97b0a5d34c1a433b442ae
    Signed-off-by: Anselme, Schubert (sa246v) <sa246v@att.com>

--- tools/helm_tk.sh
+++ tools/helm_tk.sh
@@ -17,7 +17,7 @@
 set -eux
 
 HTK_REPO=${HTK_REPO:-"https://opendev.org/openstack/openstack-helm-infra.git"}
-HTK_STABLE_COMMIT=${HTK_COMMIT:-"a7cd689280cdbc0acd04a7a1b745941260e8700b"}
+HTK_STABLE_COMMIT=${HTK_COMMIT:-"51c70e48dff173281a77d374d87af2c49caa6348"}
 
 
 
